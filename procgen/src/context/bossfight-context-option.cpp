f82f0e23d73b8f0a264c33d7f9cbd9e1a97b35bd
Yuan9815 <yuanshi9815@outlook.com>
2022-12-27 16:30:27 +0800

    1. default value of context options 2. Bossfight startup

--- procgen/src/context/bossfight-context-option.cpp
+++ procgen/src/context/bossfight-context-option.cpp
@@ -1,5 +1,6 @@
 #include "bossfight-context-option.h"
 #include "../cpp-utils.h"
+#include <iostream>
 
 BossfightContextOption::BossfightContextOption(/* args */)
 {
@@ -12,6 +13,16 @@ BossfightContextOption::~BossfightContextOption()
 
 void BossfightContextOption::parse_options(VecOptions *opts)
 {
-    // opts->consume_float("start_r", &start_r);
+    opts->consume_int("completion_bonus", &completion_bonus, true, 10);
+    opts->consume_int("positive_reward", &positive_reward, true, 1);
+
+    opts->consume_float("boss_r", &boss_r, true, 3.0);
+    opts->consume_int("player_bullet_vel", &player_bullet_vel, true, 1);
+    opts->consume_int("bottom_margin", &bottom_margin, true, 6);
+
+    opts->consume_int("boss_vel_timeout", &boss_vel_timeout, true, 50);
+    opts->consume_int("boss_damage_timeout", &boss_damage_timeout, true, 40);
+    
+    std::cout << "boss_r: " << boss_r << std::endl;
     // opts->consume_int("fish_interval", &fish_interval);
 }
--- procgen/src/context/bossfight-context-option.h
+++ procgen/src/context/bossfight-context-option.h
@@ -6,8 +6,16 @@ class BossfightContextOption
 private:
     /* data */
 public:
-    // float start_r;
-    // int fish_interval;
+    int completion_bonus;
+    int positive_reward;
+
+    float boss_r; // The radius of the boss
+    int player_bullet_vel; // The velocity of the player's bullet
+    int bottom_margin; // The bottom margin of barriers
+
+    int boss_vel_timeout; // The interval time of boss's velocity change
+    int boss_damage_timeout; // The timeout of boss's round health decrease to 0
+
 
     BossfightContextOption(/* args */);
     ~BossfightContextOption();
